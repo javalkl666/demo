8dd7c4423da62f3109d5c92b40f6ec93578479b7
javalkl666 <16619844489@163.com>
2019-10-17 17:48:28 +0800

    jenkins demo springboot

--- src/main/java/com/loomo/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/loomo/jenkinsdemo/JenkinsdemoApplication.java
@@ -2,9 +2,16 @@ package com.loomo.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsdemoApplication {
+public class JenkinsdemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsdemoApplication.class, args);
--- src/main/java/com/loomo/jenkinsdemo/controller/jenkinsController.java
+++ src/main/java/com/loomo/jenkinsdemo/controller/jenkinsController.java
@@ -1,14 +1,13 @@
 package com.loomo.jenkinsdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "jenkins")
 public class jenkinsController {
-
-    @RequestMapping(value = "index")
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String test(){
-        return "hello jenkins";
+        return "hello jenkins 111";
     }
 }
