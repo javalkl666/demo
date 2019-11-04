fa77722c65d6b8dfec83e2057f44c2852ff88e72
javalkl666 <46019422+javalkl666@users.noreply.github.com>
2019-11-04 17:30:26 +0800

    Update jenkinsController.java

--- src/main/java/com/loomo/jenkinsdemo/controller/jenkinsController.java
+++ src/main/java/com/loomo/jenkinsdemo/controller/jenkinsController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class jenkinsController {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String test(){
-        return "hello jenkins 666";
+        return "jenkins";
     }
 }
