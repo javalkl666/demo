dd9e6789b81ab7479ca6fafd13e381e1a4986566
javalkl666 <46019422+javalkl666@users.noreply.github.com>
2019-11-06 14:40:24 +0800

    Update jenkinsController.java

--- src/main/java/com/loomo/jenkinsdemo/controller/jenkinsController.java
+++ src/main/java/com/loomo/jenkinsdemo/controller/jenkinsController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class jenkinsController {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String test(){
-        return "jenkins 8888";
+        return "192.168.1.54自动部署成功";
     }
 }
